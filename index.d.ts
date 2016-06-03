3a7a97b3619080a1c3263ed941cedf61b7b01411
Miguel Andrade <jmandrade0@gmail.com>
2016-06-03 11:22:48 +0100

    extract register function and add typscript typings

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+declare module "express-decorators" {
+  import { Router, Application} from "express";
+
+  const controller: (path: string) => ClassDecorator
+
+  const route: (method: string, path: string) => MethodDecorator
+  const middleware: (fn: Function|string) => MethodDecorator
+  const use: MethodDecorator
+
+  const all: (path: string) => MethodDecorator
+  const checkout: (path: string) => MethodDecorator
+  const connect: (path: string) => MethodDecorator
+  const copy: (path: string) => MethodDecorator
+  const deleteM: (path: string) => MethodDecorator
+  const get: (path: string) => MethodDecorator
+  const head: (path: string) => MethodDecorator
+  const lock: (path: string) => MethodDecorator
+  const merge: (path: string) => MethodDecorator
+  const mkactivity: (path: string) => MethodDecorator
+  const mkcol: (path: string) => MethodDecorator
+  const move: (path: string) => MethodDecorator
+  const msearch: (path: string) => MethodDecorator
+  const notify: (path: string) => MethodDecorator
+  const options: (path: string) => MethodDecorator
+  const param: (path: string) => MethodDecorator
+  const patch: (path: string) => MethodDecorator
+  const post: (path: string) => MethodDecorator
+  const propfind: (path: string) => MethodDecorator
+  const proppatch: (path: string) => MethodDecorator
+  const purge: (path: string) => MethodDecorator
+  const put: (path: string) => MethodDecorator
+  const report: (path: string) => MethodDecorator
+  const search: (path: string) => MethodDecorator
+  const subscribe: (path: string) => MethodDecorator
+  const trace: (path: string) => MethodDecorator
+  const unlock: (path: string) => MethodDecorator
+  const unsubscribe: (path: string) => MethodDecorator
+
+  const register: (app: Router|Application, any) => void
+
+  export {
+    controller,
+
+    route, middleware, use,
+
+    all, checkout, connect, copy, deleteM as delete, get, head, lock, merge, mkactivity, mkcol, move,
+    /*msearch as "m-search",*/ notify, options, param, patch, post, propfind, proppatch, purge, put, report,
+    search, subscribe, trace, unlock, unsubscribe,
+
+    register
+  }
+}
--- index.js
+++ index.js
@@ -18,50 +18,52 @@ function trimslash(s) {
     : s;
 }
 
+function register(router, controller) {
+  if (controller.routes) {
+    var context = controller;
+
+    for (var k in controller.routes) {
+      var route = controller.routes[k];
+      var url = route.path;
+
+      if (route.method !== 'param' && _.isString(url)) {
+        url = (trimslash(controller.baseUrl) + trimslash(url)) || '/';
+      }
+
+      // var is tricky :(
+      var args = route.handlers.map((function (route, url) {
+        return function (handler) {
+          return function (request, response, next) {
+            debugHandlers(route.method.toUpperCase() + ' ' + (url ? url + ' ' : '')
+              + handler.name);
+
+            var result = handler.apply(context, arguments);
+
+            if (typeof result !== 'undefined' && result !== null && typeof result.then === 'function') {
+              result.then(undefined, next);
+            }
+          };
+        };
+      })(route, url));
+
+      debugRoutes(route.method.toUpperCase() + ' ' + (url ? url + ' ' : '')
+        + route.handlers[route.handlers.length - 1].name)
+
+      if (url) {
+        args.unshift(url);
+      }
+
+      router[route.method].apply(router, args);
+    }
+  }
+}
 
 function controller(baseUrl) {
   return function (target) {
     target.prototype.baseUrl = baseUrl;
-
     target.prototype.register = function (router) {
-      if (this.routes) {
-        var context = this;
-
-        for (var k in this.routes) {
-          var route = this.routes[k];
-          var url = route.path;
-
-          if (route.method !== 'param' && _.isString(url)) {
-            url = (trimslash(this.baseUrl) + trimslash(url)) || '/';
-          }
-
-          // var is tricky :(
-          var args = route.handlers.map((function (route, url) {
-            return function (handler) {
-              return function (request, response, next) {
-                debugHandlers(route.method.toUpperCase() + ' ' + (url ? url + ' ' : '')
-                  + handler.name);
-
-                var result = handler.apply(context, arguments);
-
-                if (typeof result !== 'undefined' && result !== null && typeof result.then === 'function') {
-                  result.then(undefined, next);
-                }
-              };
-            };
-          })(route, url));
-
-          debugRoutes(route.method.toUpperCase() + ' ' + (url ? url + ' ' : '')
-            + route.handlers[route.handlers.length - 1].name)
-
-          if (url) {
-            args.unshift(url);
-          }
-
-          router[route.method].apply(router, args);
-        }
-      }
-    };
+      register(router, this);
+    }
   };
 };
 
@@ -135,3 +137,4 @@ methods.forEach(function (method) {
 });
 
 module.exports.use = use;
+module.exports.register = register;
